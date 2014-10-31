19d797af71cc670cb57e9f9f901db06af9e76cea
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-10-31 11:06:05 -0700

    <p>

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,7 +6,7 @@
 
 	// If you get an error, it will print out an error.
 	if($connection->connect_error) {
-		die("Error: " . $connection->connect_error);
+		die("<p>Error: " . $connection->connect_error . "</p>");
 	}
 
 	// This is going to try and access a database that exists on the server.
@@ -18,19 +18,26 @@
 	
 	// And this will echo out if the database was created successfully.
 		if($query) {
-			echo "Successfully created database: " . $database;
+			echo "<p>Successfully created database: " . $database . "</p>";
 		}
 	} 
 	// This echos out that the database has already been added.
 	else {
-		echo "Database already exists.";
+		echo "<p>Database already exists.</p>";
 	}
 
 	// It's making a table post and putting an id in it automatically, a text that can only have 255 characters, and the post is text. None of them can be NULL. And the primary key is id.
 	$query = $connection->query("CREATE TABLE posts ("
 		. "id int(11) NOT NULL AUTO_INCREMENT,"
 		. "title varchar(255) NOT NULL,"
-		. "post text NOT NULL"
-		. "PRIMARY KEY (id)");
+		. "post text NOT NULL,"
+		. "PRIMARY KEY (id))");
 
+	if($query) {
+		echo "<p>Successfully create table: posts</p>";
+	}
+	else {
+		echo "<p>$connection->error</p>";
+	}
 	$connection->close();
+
