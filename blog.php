b346a1846745c3b5aa784a86e6c66d0325e6447e
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-03 12:02:59 -0800

    New files

--- blog.php
+++ blog.php
@@ -1,4 +1,6 @@
 <?php
-	// It calls the create-db.php file
+	// You insert the files you need with require_once.
+	require_once(__DIR__ . "/view/header.php");
 	require_once(__DIR__ . "/controller/create-db.php");
+	require_once(__DIR__ . "/view/footer.php");
 ?>
---
+++ view/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<!-- You can insert all the stylesheets in here.-->
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+<!-- Putting the rest in the footer file helps you to easily maintain your code. -->
+	</head>
+	<body>
