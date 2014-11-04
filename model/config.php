deddcc5c7701b3241d8032c6b3d1562d32e7d41b
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-04 11:07:47 -0800

    Keeping all the files in place

---
+++ model/config.php
@@ -0,0 +1,3 @@
+<?php
+// This is keeping all the files in one page so you won't have to update anything.
+	$path = "/blog-php/";
--- view/navigation.php
+++ view/navigation.php
@@ -1,5 +1,10 @@
+<?php
+// Inserting the config file.
+	require_once(__DIR__ . "/../model/config.php");
+?>
+<!--This is calling the post file and the $path tag -->
 <nav>
 	<ul>
-		<li><a href="../post.php">Blog Post Form</a></li>
+		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
 	</ul>
 </nav>
