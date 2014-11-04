45574abad864a8603e8d7bb8403cf234d2da03c8
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-04 10:50:12 -0800

    Changes

--- blog.php
+++ blog.php
@@ -1,6 +1,7 @@
 <?php
 	// You insert the files you need with require_once.
 	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/view/navigation.php");
 	require_once(__DIR__ . "/controller/create-db.php");
 	require_once(__DIR__ . "/view/footer.php");
 ?>
--- view/header.php
+++ view/header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<!-- You can insert all the stylesheets in here.-->
+<!-- You can insert all the stylesheets in the header file.-->
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<link rel= "stylesheet" type= "text/css" href= "blog-stylesheet.css">
 		<title></title>
 <!-- Putting the rest in the footer file helps you to easily maintain your code. -->
 	</head>
