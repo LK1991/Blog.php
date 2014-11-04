6213e9ac103b71d94229ab109619d62712db547e
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-04 11:20:18 -0800

    Commenting on nav

--- view/navigation.php
+++ view/navigation.php
@@ -2,7 +2,7 @@
 // Inserting the config file.
 	require_once(__DIR__ . "/../model/config.php");
 ?>
-<!--This is calling the post file and the $path tag -->
+<!--This is calling the $path tag so the post file can show up-->
 <nav>
 	<ul>
 		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
