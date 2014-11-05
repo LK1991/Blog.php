91d98834d46d750197ebf639f11766a18af62cf8
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-05 10:55:51 -0800

    Changes

---
+++ view/form.php
@@ -0,0 +1,13 @@
+<!-- This is creating a form -->
+<h1>Create Blog Post</h1>
+
+<form>
+	<div>
+		<label for="title">Title: </label>
+		<input type="text" name="title" />	 
+	</div>
+	<div>
+		<label for="post">Post: </label>
+		<textarea name="post"></textarea>
+	</div>
+</form>
--- view/header.php
+++ view/header.php
@@ -4,6 +4,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel= "stylesheet" type= "text/css" href= "blog-stylesheet.css">
+		<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
 		<title></title>
 <!-- Putting the rest in the footer file helps you to easily maintain your code. -->
 	</head>
