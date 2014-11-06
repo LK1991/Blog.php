cbb479be226183382fc23e66aad6f77dd7dd26c2
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-06 10:56:52 -0800

    Changes

--- view/header.php
+++ view/header.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
 <!-- You can insert all the stylesheets in the header file.-->
+<!DOCTYPE html>
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 <html>
 	<head>
 		<meta charset="UTF-8">
