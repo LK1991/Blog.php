c183d27374ef14a7e786f840dd78a01b662f7718
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-13 11:12:02 -0800

    Inserting

--- controller/create-post.php
+++ controller/create-post.php
@@ -2,14 +2,22 @@
 	// It's calling the database.php file and everything in it.
 	require_once(__DIR__ . "/../model/database.php");
 
-	$connection = new mysqli($host, $userame, $password, $database);
+	$connection = new mysqli($host, $username, $password, $database);
 
 	// There are filtering the input, getting it from a post that we are submitting, make it so we're getting it from title and post, and it's filtering it making sure it's a string.
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: $post</p>";
+	// This is inserting the post that we put into the database.
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+	// If inserting it was successfull, it will echo out that and if it wasn't, it'll echo out an error.
+	if($query) {
+		echo "<p>Successfully inserted post: $title</p>";
+	}
+	else {
+		echo "<p>$connection->error</p>";
+	}
 
 	$connection->close();
 	// That is closing the connection.
