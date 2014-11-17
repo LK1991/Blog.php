e2476b8efaf4b3593c3bd6016ba7d729d7d96f90
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-17 11:47:07 -0800

    c

--- model/database.php
+++ model/database.php
@@ -8,4 +8,11 @@ class Database {
 	private $username;
 	private $password;
 	private $database;
+
+	public function __construct($host, $username, $password, $database) {
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	}
 }
