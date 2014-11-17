964bf1040b53e9db866d3b0030b675d0ea2af878
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-17 12:05:54 -0800

    Functions

--- model/database.php
+++ model/database.php
@@ -9,10 +9,23 @@ class Database {
 	private $password;
 	private $database;
 
+// 
 	public function __construct($host, $username, $password, $database) {
 		$this->host = $host;
 		$this->username = $username;
 		$this->password = $password;
 		$this->database = $database;
+	}
+
+	public function openConnection() {
+
+	}
+
+	public function closeConnection() {
+
+	}
+
+	public function query($string) {
+
 	}
 }
