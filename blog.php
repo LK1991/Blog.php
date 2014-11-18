66c156aaa9d73b1d75ffd4a1a8717045c2a0a072
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-11-18 11:26:52 -0800

    Date

--- blog.php
+++ blog.php
@@ -4,4 +4,8 @@
 	require_once(__DIR__ . "/view/navigation.php");
 	require_once(__DIR__ . "/controller/create-db.php");
 	require_once(__DIR__ . "/view/footer.php");
+
+	$date = new DateTime('today');
 ?>
+
+<p>	The output date is: <?php echo $date->format("m.d.y"); ?></p>
