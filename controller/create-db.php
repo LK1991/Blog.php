ea32948626441061bb5b2d9d42035309c29515a8
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-12-03 11:20:37 -0800

    Css and new table and session

--- controller/create-db.php
+++ controller/create-db.php
@@ -17,3 +17,22 @@
 		echo "<p>" . $_SESSION["connection"]->error . "</p>";
 	}
 	// A session is a way to store information in variables to be used across multiple pages.
+	// And this echos if it was created successfully.
+
+	// It's making a table post and putting an id in it automatically, a text that can only have 255 characters, and the post is text. None of them can be NULL. And the primary key is id.
+	$query = $_SESSION["connection"]->query("CREATE TABLE users ("
+		. "id int(11) NOT NULL AUTO_INCREMENT,"
+		. "username varchar(30) NOT NULL,"
+		. "email varchar(50) NOT NULL,"
+		. "password char(128) NOT NULL,"
+		. "salt char(128) NOT NULL,"
+		. "PRIMARY KEY (id))");
+
+	if($query) {
+		echo "<p>Successfully created table:users</p>";
+	}
+	else {
+		echo "<p>" . $_SESSION["connection"]->error . "</p>";
+	}
+	// A session is a way to store information in variables to be used across multiple pages.
+	// And this echos if it was created successfully.
