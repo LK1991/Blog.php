f9e876775a6fbbc923ed916152d8f253bc4aa489
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-12-08 12:04:57 -0800

    Comments

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,8 +1,11 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php");
 
+	//  The FILTER_SANITIZE_EMAIL removes all illegal e-mail characters from a string.
+	// The FILTER_SANITIZE_STRING strips or encodes unwanted characters.
 	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
+	// This is echoing out what you put in the register form.
 	echo $email . " - " . $username . " - " . $password;
