7abb34fcd85aed58a9c9defc1c6d53ddbc116976
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-12-08 12:00:57 -0800

    DateTime

--- controller/create-post.php
+++ controller/create-post.php
@@ -9,6 +9,8 @@
 	// There are filtering the input, getting it from a post that we are submitting, make it so we're getting it from title and post, and it's filtering it making sure it's a string.
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+	$date = new DateTime('today');
+	$time = new DateTime('America/Los_Angeles');
 
 	// This is inserting the post that we put into the database.
 	$query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title', post = '$post'");
@@ -16,6 +18,7 @@
 	// If inserting it was successfull, it will echo out that is was successful and if it wasn't, it'll echo out an error.
 	if($query) {
 		echo "<p>Successfully inserted post: $title</p>";
+		echo "Posted on: " . $date->format("M/d/y") . " at " . $time->format("g:i");
 	}
 	else {
 		echo "<p>" . $_SESSION["connection"]->error . "</p>";
