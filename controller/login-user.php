87ed94be343e161fa5017ae2ff9d184c8498d27a
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-12-10 11:16:51 -0800

    Sanitizing the username and password and selecting the salt and password

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,2 +1,10 @@
 <?php
-	echo "Successfully logged in user";
+
+	require_once(__DIR__ . "/../model/config.php");
+
+	// The FILTER_SANITIZE_STRING strips or encodes unwanted characters.
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+	// This is selecting the salt and the password from the users database in the username column.
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE usernames = '$username'");
