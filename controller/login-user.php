738a70cd2d470f4a74294d406d85b70c85695771
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-12-15 12:03:44 -0800

    fixed error

--- controller/login-user.php
+++ controller/login-user.php
@@ -14,7 +14,7 @@
 		$row = $query->fetch_array();
 
 		if($row["password"] === crypt($password, $row["salt"])) {
-			$_SESSION["authentciated"] = true;
+			$_SESSION["authenticated"] = true;
 			echo "<p>Login Successful!</p>";
 		}
 		else {
