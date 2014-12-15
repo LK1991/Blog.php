973c8b00af18d9f9700f118f0e50c1935a6ef83c
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-12-15 00:10:29 -0800

    Authenticated user comments and location

--- view/form.php
+++ view/form.php
@@ -1,6 +1,13 @@
 <?php
 	// Requiring config.php
 	require_once(__DIR__ . "/../model/config.php");
+	require_once(__DIR__ ."/../controller/login-verify.php");
+
+	// If a user is authenticated, it will take you back to the blog page.
+	if(!authenticateUser()) {
+		header("Location: ". $path . "blog.php");
+		die();
+	}
 ?>
 
 <!-- This is creating a form that you can submit to the project in phpMyAdmin -->
--- view/navigation.php
+++ view/navigation.php
@@ -1,6 +1,13 @@
 <?php
 // Inserting the config file.
 	require_once(__DIR__ . "/../model/config.php");
+	require_once(__DIR__ ."/../controller/login-verify.php");
+
+	//If a user is authenticated, it will take you back to the blog page.
+	if(!authenticateUser()) {
+		header("Location: ". $path . "blog.php");
+		die();
+	}
 ?>
 <!--This is calling creating a path to post.php-->
 <nav>
