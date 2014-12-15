d900b71b536b99651964fa7da34cfd88a1896297
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2014-12-15 00:21:06 -0800

    logout user

---
+++ controller/logout-user.php
@@ -0,0 +1,8 @@
+<?php
+	// Requiring files.
+	require_once(__DIR__ . "/../model/config.php");
+
+	unset($_SESSION["authenticated"]);
+
+	session_destroy();
+	header("Location: " . $path . "blog.php");
